abf0a3ba75741eca2c0212e7e5ef7177ac80d49e
CathyCahy <cathycahy@gmail.com>
2024-04-08 20:55:14 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        int year = 2025;
+        determinateIeapYear(year);
+
+        //Задача 2
+        int clientOS = 0;
+        int clientDeviceYear = 2004;
+        determinateVersionOS(clientOS, clientDeviceYear);
+
+        //Задача 3
+        int distance = 100;
+        int deliveryDays = calculateDeliveryDays(distance);
+        if (deliveryDays > 0) {
+            System.out.println("Доствка займет " + deliveryDays + " дней");
+        } else System.out.println("Доставки нет");
+    }
+
+    //Метод для задачи 1 - определение високосный год или нет
+    static void determinateIeapYear(int year) {
+        if (year >= 1584) {
+            if ((year % 4 == 0) && (year % 100 != 0)) {
+                System.out.println(year + " год является високосным");
+            } else System.out.println(year + " год не является високосным");
+        } else System.out.println(year + " год не является високосным");
+    }
+
+    //Метод для задачи 2
+    static void determinateVersionOS(int clientOS, int clientDeviceYear) {
+        if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else System.out.println("Установите версию приложения для iOS по ссылке");
+        }
 
     }
+
+    //Метод для задачи 3
+    static int calculateDeliveryDays(int deliveryDistance) {
+
+        if (deliveryDistance <= 20) {
+            return 1;
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            return 2;
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            return 3;
+        } else {
+            return (-1);
+        }
+    }
+
 }
