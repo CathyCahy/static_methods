bf2835701fc626dc5c11910ff1925e7d8e841435
CathyCahy <cathycahy@gmail.com>
2024-04-09 09:15:27 +0300

    homework done 2

--- src/Main.java
+++ src/Main.java
@@ -28,15 +28,6 @@ public class Main {
 
     //Метод для задачи 2
     static void determinateVersionOS(int clientOS, int clientDeviceYear) {
-//        if (clientOS == 1) {
-//            if (clientDeviceYear < 2015) {
-//                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-//            } else System.out.println("Установите версию приложения для Android по ссылке");
-//        } else if (clientOS == 0) {
-//            if (clientDeviceYear < 2015) {
-//                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-//            } else System.out.println("Установите версию приложения для iOS по ссылке");
-//        }
         if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1 && clientDeviceYear > 2015) {
